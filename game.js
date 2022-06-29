3355256b684d8e1a0e19e741c15e8210eed0f147
Parker Bullington-McGuire <pbullingtonmcguire@gmail.com>
2022-06-29 17:07:47 -0400

    made the draw card button draw and display cards

--- game.js
+++ game.js
@@ -7,11 +7,20 @@ const getDeck = async () => {
     ))
 }
 
-const drawCard = () => {
-    cardDisplay.innerHTML = '<p>test</p>'
+const drawCard = async (deckId) => {
+    console.log(deckId)
+    newCard = await axios.get(
+        `http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+    )
+    newCard = newCard.data.cards[0]
+    console.log(newCard)
+    cardImage = newCard.images.png
+    console.log(cardImage)
+    cardDisplay.innerHTML += `<img src="${cardImage}"></img>` 
+    console.log(cardDisplay.innerHTML)
 }
 getDeck().then((value) => {
     console.log(value)
-    deckId = value.deck_id
-    drawButton.addEventListener('click', drawCard)
+    deckId = value.data.deck_id
+    drawButton.addEventListener('click', () => (drawCard(deckId)))
 })
