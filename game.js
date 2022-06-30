3e96428b01a3c52e6c6279207cfef367290e98ae
Parker Bullington-McGuire <pbullingtonmcguire@gmail.com>
2022-06-29 21:04:25 -0400

    made the buttons invisible by default and toggleable

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 const drawButton = document.querySelector('button[id="drawCard"')
+const standButton = document.querySelector('button[id="stand"')
 const cardDisplay = document.querySelector('div[id="cardDisplay')
 const totalDisplay = document.querySelector('h3[id="totalDisplay"]')
-const standButton = document.querySelector('button[id="stand"')
+
 
 let playerTotal = 0
 
@@ -15,18 +16,18 @@ const drawCard = async (deckId) => {
     //console.log(deckId)
     let newCard = await axios.get(
         `http://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-    )
-    newCard = newCard.data.cards[0]
-    // console.log(newCard)
-    cardImage = newCard.images.png
-    // console.log(cardImage)
-    cardDisplay.innerHTML += `<img src="${cardImage}"></img>` 
-    //console.log(cardDisplay.innerHTML)
-    let faceValue
-    switch(newCard.value) {
-        case 'ACE': faceValue = 11
-        break
-        case 'JACK':
+        )
+        newCard = newCard.data.cards[0]
+        // console.log(newCard)
+        cardImage = newCard.images.png
+        // console.log(cardImage)
+        cardDisplay.innerHTML += `<img src="${cardImage}"></img>` 
+        //console.log(cardDisplay.innerHTML)
+        let faceValue
+        switch(newCard.value) {
+            case 'ACE': faceValue = 11
+    break
+    case 'JACK':
         case 'QUEEN':
         case 'KING': faceValue = 10
         break
@@ -51,7 +52,7 @@ const playerDraw = async (deckId) => {
 
 const hit = async (deckId) => {
     const cardValue = await playerDraw(deckId)
-
+    
 }
 
 const stand = async () => {
@@ -62,9 +63,21 @@ const playAgain = () => {
     console.log('playAgain() not yet implemented')
 }
 
+const toggleHitAndStand = (toggle) => {
+    let displayStyle
+    if (toggle) {
+        displayStyle = 'inline'
+    } else {
+        displayStyle = 'none'
+    }
+    drawButton.style.display = displayStyle
+    standButton.style.display = displayStyle
+}
+
 getDeck().then((value) => {
     console.log(value)
     deckId = value.data.deck_id
+    toggleHitAndStand(true)
     drawButton.addEventListener('click', () => {playerDraw(deckId)})
     standButton.addEventListener('click', stand)
 })
